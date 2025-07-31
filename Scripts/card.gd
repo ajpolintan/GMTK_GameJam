3ef4eb68033f03b5b161faea8cc77931b5f475fc
Allen Jake Polintan <ajpolintan@dons.usfca.edu>
2025-07-31 13:59:56 -0700

    Imported Deck Code

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -1,12 +1,13 @@
 class_name Card extends Node2D
 
+
 @export var card_name : String = "Card Name"
 @export var card_description : String = "Card Description"
 @export var card_image: Sprite2D
 
 @onready var name_label : Label = $CardName/NameLabel
 @onready var description_label : Label = $CardDescription
-
+@onready var baseSprite : Sprite2D = $BaseCard
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setCardValues(card_name, card_description)
@@ -22,6 +23,20 @@ func setCardValues(_name: String, _description: String):
 func _process(delta: float) -> void:
 	pass
 
-
+func unhighlight():
+	baseSprite.set_modulate(Color(1, 1, 1, 1))
+	
+func highlight():
+	baseSprite.set_modulate(Color(0.5, 1, 1 , 0.8))
+		
+	
 func _on_area_2d_mouse_entered() -> void:
+	highlight() # Replace with function body.
+
+
+func _on_area_2d_mouse_exited() -> void:
+	unhighlight()
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	print(card_name)
 	pass # Replace with function body.
