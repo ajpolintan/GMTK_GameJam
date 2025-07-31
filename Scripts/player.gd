aea0f06907177489b81b08dfc3a706b0cefaf2e7
Mushroomyploom <lewis.w.kynan@gmail.com>
2025-07-30 22:42:05 -0700

    chesticles

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var jumping = false
-const SPEED = 120.0
-const JUMP_VELOCITY = -180.0
-const GRAVITY = 18.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -170.0
+const GRAVITY = 20.0
+const MAX_DOWN = 200
 
 func _on_timer_timeout():
 	jumping = false
@@ -14,7 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() && !jumping && not Input.is_action_pressed("jump"):
 		if velocity.y < -50: velocity.y = -50
-		else: velocity.y = move_toward(velocity.y, -JUMP_VELOCITY, GRAVITY)
+		else: velocity.y = move_toward(velocity.y, MAX_DOWN, GRAVITY)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") && is_on_floor():
@@ -24,7 +25,7 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("jump") && jumping:
 		velocity.y = JUMP_VELOCITY
 	elif Input.is_action_pressed("jump") && !jumping:
-		velocity.y = move_toward(velocity.y, -JUMP_VELOCITY, GRAVITY)
+		velocity.y = move_toward(velocity.y, MAX_DOWN, GRAVITY)
 	elif jumping: jumping = false
 	
 	# Get the input direction and handle the movement/deceleration.
