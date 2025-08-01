4190385a6e97ee19f25bd749cbab43d5be040451
Allen Jake Polintan <ajpolintan@dons.usfca.edu>
2025-07-31 19:37:15 -0700

    Added select sfx

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var sfx_select = $Select
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,17 +10,21 @@ func _ready() -> void:
 
 func _on_start_pressed() -> void:
 	print("Start pressed")
+	sfx_select.play()
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 	pass # Replace with function body.
 
 
 func _on_options_pressed() -> void:
 	print("Settings pressed")
+	sfx_select.play()
+
 
 	pass # Replace with function body.
 
 
 func _on_quit_pressed() -> void:
+	sfx_select.play()
 	print("Exit pressed")
 	get_tree().quit()
 
