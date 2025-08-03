0c8850b3d05ce867f506b0b24644c6a5bdd1c567
Mushroomyploom <lewis.w.kynan@gmail.com>
2025-08-02 23:10:34 -0700

    camera

---
+++ Scripts/camera.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+func _ready():
+	limit_top = -1100
+	limit_bottom = 50
+
+
+func _on_game_forest() -> void:
+	limit_top = -1100
+	limit_bottom = 50
+	
+func _on_game_clouds() -> void:
+	limit_top = -1560
+	limit_bottom = 50
+
+func _on_game_lab() -> void:
+	limit_top = -1180
+	limit_bottom = -94
